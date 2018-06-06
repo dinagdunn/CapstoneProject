78852e7a1f51ee57014700067456ea14fe3cd6a5
syntel <syntel@PHX440G3-2815YY.syntelorg.com>
2018-06-06 16:42:26 -0400

    Added search failure function

--- baymanagement/src/Search.js
+++ baymanagement/src/Search.js
@@ -3,30 +3,57 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
 import LoadPallet from './LoadPallet.js'
 import LoadBay from './LoadBay.js'
+import SearchFail from './SearchFail.js'
 
 class Search extends Component {
-	
-	constructor() {
-		super();
-		this.clickHandler = this.clickHandler.bind(this)
- }
 
-	clickHandler(event) {
-		event.preventDefault();
-		const query = event.target[0].value
-		this.props.history.push(`/search/${query}`)
-}
+    constructor() {
+        super();
+        this.clickHandler = this.clickHandler.bind(this)
+        this.state = {
+            search: true
+        }
+    }
 
-	render() {
-		return (
-			<form onSubmit={this.clickHandler} className="bar">
-				<input type="text" />
-				<button className="btn btn-primary" type="submit">Search</button>
-			</form>
+    clickHandler(event) {
+        event.preventDefault();
+      
+        
+        const query = event.target[0].value
+           
+        if (query[0].toUpperCase() === "P" || query[0].toUpperCase() === "B") {
+            
+  
+                this.props.history.push(`/search/${query}`)
+        
+
+        }
+        else {
+            document.querySelector('[data-error]').style.display = "block";
+            
+
+        }
+    }
+
+    render() {
+
+        return (
+            <React.Fragment>
+            <form onSubmit={this.clickHandler} className="bar">
+                <input type="text"/>
+                
+
+                <button className="btn btn-primary" type="submit">Search</button>
+               
+            </form>
+            <div style={{display: "none"}} data-error>
+                <h3>Incorrect value. Please try again.</h3>
+                </div>
+                </React.Fragment>
 
 
-			)
-	}
+        )
+    }
 }
 
 export default Search
