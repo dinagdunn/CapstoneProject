d1aa13ba21ffa93bbcf54cfa631409257033f44a
newsha400 <newsha400@gmail.com>
2018-06-06 14:17:41 -0400

    masterbay controller and dao

--- BayPalleteLookUp/src/main/java/bootcamp/dao/MasterBayDao.java
+++ BayPalleteLookUp/src/main/java/bootcamp/dao/MasterBayDao.java
@@ -8,6 +8,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import bootcamp.model.Bay;
+import bootcamp.model.MasterBay;
 
 @Component
 public class MasterBayDao {
@@ -15,10 +16,20 @@ public class MasterBayDao {
 	@Autowired
 	JdbcTemplate jdbctemplate;
 
-	private final String GET_MASTERBAY_LIST = "SELECT DISTINCT masterbay FROM baymanagement.bay";
+	private final String GET_MASTERBAY_LIST = "SELECT * FROM masterbay;";
+	private final String GET_BAY_LIST = "SELECT * FROM Bay WHERE masterbay =?;";
+	private final String GET_MASTERBAY = "SELECT * FROM masterbay WHERE id = ?;";
 	
-	public List<Bay> getMasterBayList() {
-		return jdbctemplate.query(GET_MASTERBAY_LIST, new BeanPropertyRowMapper<>(Bay.class));
+	public List<MasterBay> getMasterBayList() {
+		return jdbctemplate.query(GET_MASTERBAY_LIST, new BeanPropertyRowMapper<>(MasterBay.class));
+	}
+	
+	public MasterBay getMasterbayById(String masterbay) {
+		Object[] args = {masterbay};
+		List<Bay> bayList = jdbctemplate.query(GET_BAY_LIST, args, new BeanPropertyRowMapper<>(Bay.class));
+		MasterBay masterBay = jdbctemplate.query(GET_MASTERBAY, args, new BeanPropertyRowMapper<>(MasterBay.class)).get(0);
+		
+		return new MasterBay(masterBay.getWidth(), masterBay.getHeight(), masterBay.getLength(), bayList);
 	}
 	
 }
