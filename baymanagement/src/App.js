8a49903680775840f3f2a503601ae61449943f97
syntel <syntel@PHX440G3-2815Z9.syntelorg.com>
2018-06-06 09:44:32 -0400

    wednesday morning

--- baymanagement/src/App.js
+++ baymanagement/src/App.js
@@ -3,6 +3,11 @@ import { Link, BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
 import Search from './Search.js'
 import Add from './Add.js'
+import LoadBay from './LoadBay.js'
+import LoadPallet from './LoadPallet.js'
+import SearchFail from './SearchFail.js'
+import EditPallet from './EditPallet.js'
+import EditBay from './EditBay.js'
 
 class App extends Component {
 
@@ -28,7 +33,7 @@ class App extends Component {
     <switch>
     <div className="col-sm-6 col-sm-offset-3">
       <div className="col-xs-4 headerButton"><Link to="/search">Search</Link></div>
-      <div className="col-xs-4 headerButton">Image</div>
+      <div className="col-xs-4 logo"><img src={require('./images/logo.jpg')} /></div>
       <div className="col-xs-4 headerButton"><Link to="/add">Add</Link></div>
     </div>
     </switch>
@@ -41,6 +46,10 @@ class App extends Component {
 
     <Route exact path="/add" component={ Add } />
     <Route exact path="/search" component={ Search } />
+    <Route path="/search/P*" component={ LoadPallet } />
+    <Route path="/search/B*" component={ LoadBay } />
+    <Route path="/edit/P*" component={ EditPallet } />
+    <Route path="/edit/B*" component={ EditBay } />    
 
     <Route exact path ="/" render={() =>
       <p className="App-intro">
